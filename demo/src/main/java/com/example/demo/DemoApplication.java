ff62929c1ed4b5cbea8d32cfa861c2cdfb2bff25
mishra-abhish <160416802+ABHISHEK-MISHR@users.noreply.github.com>
2024-06-12 12:38:33 +0530

    spring test1

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		System.out.println("i am here");
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("hello world spring");
+		System.out.println("hello world spring test");
 	}
 
 }
